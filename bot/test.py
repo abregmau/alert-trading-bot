785c95342872b8b897b47acf1ad779c7811431e3
abregmau <mauricio.matias93@gmail.com>
2022-07-30 19:33:35 -0300

    Test for deploy in Heroku

--- bot/test.py
+++ bot/test.py
@@ -8,6 +8,8 @@ import time
 bot = telebot.TeleBot(token=os.environ['TOKEN'])
 i=0
 
+bot.send_message(353032690, 'Start!')
+
 # Send message
 while(True):
     text= 'Hi! I\'m a Bot! by Mauricio Abregú ' + str(i)
